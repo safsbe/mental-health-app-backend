ecc86545c0fda22aed49b354dcb9962822d892e1
Rifa Achrinza <25147899+achrinza@users.noreply.github.com>
2025-02-03 11:37:28 +0800

    fix: dev db seeding
    Signed-off-by: Rifa Achrinza <25147899+achrinza@users.noreply.github.com>

--- api2/resources/db/migration/0001-populate.sql
+++ api2/resources/db/migration/0001-populate.sql
@@ -7,9 +7,9 @@ VALUES ('Category 1', 'https://blob.dev.lumin.itserv.com.sg/c82210cb-3a7c-4b24-9
 INSERT INTO "articles" ("category_id", "title", "creator_id", "updater_id")
 VALUES (1, 'Article 1', 1, 1);
 
-INSERT INTO "article_pages" ("article_id", "page_index", "body")
-VALUES (1, 1, 'Article 1 Page 1'),
-       (1, 2, 'Article 1 Page 2');
+INSERT INTO "article_pages" ("article_id", "page_index", "title", "body")
+VALUES (1, 1, 'Aritcle 1 Page 2 Title', 'Article 1 Page 1 Body'),
+       (1, 2, 'Article 1 Page 2 Title', 'Article 1 Page 2 Body');
 
 INSERT INTO "meditation_tracks" ("title", "file_location")
 VALUES ('Meditation Track 1', 'https://blob.dev.lumin.itserv.com.sg/364b8206-b24d-406e-9ae8-8e0ab90e013a');
